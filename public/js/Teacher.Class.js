05d40727eca544865d8069e40124f720570a6936
Alexey <lex10000@yandex.ru>
2020-05-14 15:52:42 +0700

    Занятие 6

--- public/js/Teacher.Class.js
+++ public/js/Teacher.Class.js
@@ -2,7 +2,7 @@ class Teacher extends Person
 {
     constructor(params) {
         super(params);
-        this.subject = params['subject'];
-        this.degree = params['degree'];
+        this.subject = params['subject'] || '';
+        this.degree = params['degree'] || '';
     }
 }
